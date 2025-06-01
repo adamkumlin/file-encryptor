52960c5dacaba946936afd631c6d319f12be5e6d
Adam Kumlin <skola.adamkumlin99@gmail.com>
2025-06-02 00:12:09 +0200

    feat: Add letters map

--- file-encryptor.cpp
+++ file-encryptor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
 
 int main()
@@ -27,5 +28,41 @@ int main()
 }
 
 static void encryptString(string contents) {
+	map<char, int> letters = {
+		{'a', 0},
+		{'b', 1},
+		{'c', 2},
+		{'d', 3},
+		{'e', 4},
+		{'f', 5},
+		{'g', 6},
+		{'h', 7},
+		{'i', 8},
+		{'j', 9},
+		{'k', 10},
+		{'l', 11},
+		{'m', 12},
+		{'n', 13},
+		{'o', 14},
+		{'p', 15},
+		{'q', 16},
+		{'r', 17},
+		{'s', 18},
+		{'t', 19},
+		{'u', 20},
+		{'v', 21},
+		{'w', 22},
+		{'x', 23},
+		{'y', 24},
+		{'z', 25}
+	};
 
+	string encrypted = "";
+
+	for (int i = 0; i < contents.length(); i++)
+	{
+		char letter = contents[i];
+		int letterIndex = letters.at(letter);
+
+	}
 }
